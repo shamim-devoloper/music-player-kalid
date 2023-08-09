36fb70ceafae6fd38efebacd10a3797ff09a5c6f
shamimislam <134716211+shamim-devoloper@users.noreply.github.com>
2023-08-09 19:52:50 +0600

    Add files via upload

---
+++ css/js/app.js
@@ -0,0 +1,9 @@
+const audio = document.querySelector(".audio");
+const play_pause = document.querySelector(".play_pause");
+
+const playPause = () => {
+  console.log("play_pause");
+};
+play_pause.addEventListener("click",{
+    alert("hi")
+})
